fb2b533d51d59c8cb39c18e01962d95977a72121
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2020-12-05 10:23:06 -0800

    added restful api functions

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, jsonify
 from model import db
 
 app = Flask(__name__)
@@ -18,6 +18,22 @@ def welcome():
 def card_view(index):
     try:
         card = db[index]
-        return render_template("card.html", card=card, index=index, max_index=len(db) - 1)
+        return render_template("card.html",
+                               card=card,
+                               index=index,
+                               max_index=len(db) - 1)
+    except IndexError:
+        abort(404)
+
+
+@app.route("/api/card/")
+def api_card_list():
+    return jsonify(db)
+
+
+@app.route('/api/card/<int:index>')
+def api_card_detail(index):
+    try:
+        return db[index]
     except IndexError:
         abort(404)
